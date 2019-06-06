34d998e5ac50411a3fb322d3fe50b09f20117dc7
Ted Semashov <tsema@softserveinc.com>
2019-06-06 17:04:49 +0300

    Added input and some functionality

--- src/names/names.js
+++ src/names/names.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import { Button } from 'react-bootstrap';
+import FormControl from 'react-bootstrap/FormControl'
+import InputGroup from 'react-bootstrap/InputGroup';
 import './names.css';
 
 class Names extends Component {
    state = {
-      names: []
+      names: [],
+      selectedName: '',
+      selectedId: '',
+      selectedSex: ''
    };
 
    componentDidMount() {
@@ -13,17 +18,57 @@ class Names extends Component {
            .then(({ data } ) => this.setState({names: data}))
    }
 
-   clickHandler = (name) => {
-     alert(name)
+   clickHandler = (name, id, sex) => {
+      this.setState({selectedName: name});
+      this.setState({selectedId: id});
+      this.setState({selectedSex: sex});
+      let names = this.state.names;
+      for (let obj of names) {
+         if(obj.name === name) {
+            names.splice(names.indexOf(obj), 1);
+         }
+      }
+      debugger;
+   };
+
+   filterNames = (e) => {
+      debugger;
+   };
+
+   cancelName = () => {
+      let newName = {
+         id: this.state.selectedId,
+         name: this.state.selectedName,
+         sex: this.state.selectedSex
+      };
+      let names = this.state.names;
+      names.unshift(newName);
+      this.setState({selectedName: ''});
+      this.setState({selectedId: ''});
+      this.setState({selectedSex: ''});
    };
 
    render() {
       const {names} = this.state;
       return (
            <div>
-              {names.map(({id, name}) =>
-                   <Button variant="outline-primary" size="sm" type="button"
-                           key={id} onClick = {(e) => this.clickHandler(name, e)}> {name} </Button>
+              <div id="filterInput">
+                 <InputGroup className="mb-3">
+                    <FormControl
+                         placeholder="Tap here for filtering"
+                         aria-label="Tap here for filtering"
+                         aria-describedby="basic-addon2"
+                         value={this.state.selectedName}
+                         onChange={this.filterNames}
+                    />
+                    <InputGroup.Append>
+                       <Button variant="outline-danger" onClick = {this.cancelName}>X</Button>
+                    </InputGroup.Append>
+                 </InputGroup>
+              </div>
+              {names.map(({id, name, sex}) =>
+                   <Button variant="outline-primary" size="sm" type="button" id="buttonName"
+                           key={id} onClick = {(e) => this.clickHandler(name, id, sex, e)}> {name} </Button>
               )
               }
            </div>
