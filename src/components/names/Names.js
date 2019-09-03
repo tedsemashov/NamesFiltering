6e0f8c39f350407cea2f8798c6ffbd5b7afcaf86
Ted Semashov <tsema@softserveinc.com>
2019-09-04 02:47:37 +0300

    almost finished functionality

--- src/components/names/Names.js
+++ src/components/names/Names.js
@@ -14,17 +14,48 @@ class Names extends Component {
    render() {
       return (
            <div>
-              <Pagination activities = {[{id:'1', name:'something'},{id:'2', name:'something'},{id:'3', name:'something'},
-                 {id:'4', name:'something'},{id:'5', name:'something'},{id:'6', name:'something'},{id:'7', name:'something'},
-                 {id:'8', name:'something'},{id:'9', name:'something'},{id:'10', name:'something'},{id:'11', name:'something'},
-                 {id:'12', name:'something'},{id:'13', name:'something'},{id:'14', name:'something'},{id:'15', name:'something'},
-                 {id:'16', name:'something'},{id:'17', name:'something'},{id:'18', name:'something'},{id:'19', name:'something'},
-                 {id:'20', name:'something'},{id:'21', name:'something'},{id:'22', name:'something'},{id:'23', name:'something'},
-                 {id:'24', name:'something'},{id:'25', name:'something'},{id:'26', name:'something'},{id:'27', name:'something'},
-                 {id:'28', name:'something'},{id:'29', name:'something'},{id:'30', name:'something'},{id:'31', name:'something'},
-                 {id:'32', name:'something'},{id:'33', name:'something'},{id:'34', name:'something'},{id:'35', name:'something'},
-              {id:'36', name:'something'},{id:'37', name:'something'},{id:'38', name:'something'},{id:'39', name:'something'},                      {id:'40', name:'something'},{id:'41', name:'something'},{id:'42', name:'something'},{id:'43', name:'something'},{id:'44', name:'something'},{id:'45', name:'something'},{id:'46', name:'something'},{id:'47', name:'something'}]}
-                          onSelect = {(arr) => this.partOfActivities(arr)}
+              <Pagination
+                   activities={[{id: '1', name: 'something'}, {id: '2', name: 'something'}, {id: '3', name: 'something'},
+                      {id: '4', name: 'something'}, {id: '5', name: 'something'}, {id: '6', name: 'something'}, {
+                         id: '7',
+                         name: 'something'
+                      },
+                      {id: '8', name: 'something'}, {id: '9', name: 'something'}, {id: '10', name: 'something'}, {
+                         id: '11',
+                         name: 'something'
+                      },
+                      {id: '12', name: 'something'}, {id: '13', name: 'something'}, {id: '14', name: 'something'}, {
+                         id: '15',
+                         name: 'something'
+                      },
+                      {id: '16', name: 'something'}, {id: '17', name: 'something'}, {id: '18', name: 'something'}, {
+                         id: '19',
+                         name: 'something'
+                      },
+                      {id: '20', name: 'something'}, {id: '21', name: 'something'}, {id: '22', name: 'something'}, {
+                         id: '23',
+                         name: 'something'
+                      },
+                      {id: '24', name: 'something'}, {id: '25', name: 'something'}, {id: '26', name: 'something'}, {
+                         id: '27',
+                         name: 'something'
+                      },
+                      {id: '28', name: 'something'}, {id: '29', name: 'something'}, {id: '30', name: 'something'}, {
+                         id: '31',
+                         name: 'something'
+                      },
+                      {id: '32', name: 'something'}, {id: '33', name: 'something'}, {id: '34', name: 'something'}, {
+                         id: '35',
+                         name: 'something'
+                      },
+                      {id: '36', name: 'something'}, {id: '37', name: 'something'}, {id: '38', name: 'something'}, {
+                         id: '39',
+                         name: 'something'
+                      }, {id: '40', name: 'something'}, {id: '41', name: 'something'}, {id: '42', name: 'something'},
+                      {id: '43', name: 'something'}, {id: '44', name: 'something'}, {id: '45', name: 'something'},
+                      {id: '46', name: 'something'}, {id: '47', name: 'something'}]}
+
+                   onSelect={(arr) => this.partOfActivities(arr)}
               />
            </div>
       );
--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -3,106 +3,118 @@ import styles from './Pagination.module.css';
 
 class Pagination extends Component {
 
-    state = {
-        allPagesList: [],
-        currentPagesList: [],
-        selectedPage: 1,
-        start: true,
-        end: false
-    };
+   state = {
+      allPagesList: [],
+      currentPagesList: [],
+      selectedPage: 1,
+      start: true,
+      end: false
+   };
 
-    componentDidMount() {
-        const pagesAmount = this.pagesAmountArray();
-        this.setState({allPagesList: pagesAmount});
-        if(pagesAmount.length > 4) {
-            this.setState({currentPagesList: pagesAmount.slice(0,4)})
-        }
-    }
+   componentDidMount() {
+      const pagesAmount = this.pagesAmountArray();
+      this.setState({allPagesList: pagesAmount});
+      if (pagesAmount.length > 4) {
+         this.setState({currentPagesList: pagesAmount.slice(0, 4)})
+      }
+   }
 
-    pagesAmountArray = () => {
-        const { activities } = this.props;
-        const pagesAmount = Math.ceil(activities.length / 9);
-        const newArr = [];
-        for (let i = 1; i <= pagesAmount; i++) {
-            newArr.push(i);
-        }
-        return newArr;
-    };
+   pagesAmountArray = () => {
+      const {activities} = this.props;
+      const pagesAmount = Math.ceil(activities.length / 9);
+      const newArr = [];
+      for (let i = 1; i <= pagesAmount; i++) {
+         newArr.push(i);
+      }
+      return newArr;
+   };
 
-    partOfActivities = (item) => {
-        const { activities } = this.props;
-        const end = item * 9;
-        const start = end - 9;
-        this.setState({selectedPage: item});
-        this.props.onSelect(activities.slice(start, end));
-    };
+   partOfActivities = (item) => {
+      const {activities} = this.props;
+      const end = item * 9;
+      const start = end - 9;
+      this.setState({selectedPage: item});
+      this.props.onSelect(activities.slice(start, end));
+   };
 
-    renderPages = () => {
-            return this.state.currentPagesList.map((item) =>
-                 item === this.state.selectedPage
+   renderPages = () => {
+      const {selectedPage} = this.state;
+      return this.state.currentPagesList.map((item) =>
+           item === selectedPage
+                ?
+                <li key={item} className={styles.selectedState} onClick={() =>
+                     this.partOfActivities(item)}> {item} </li>
+                :
+                <li key={item} onClick={() => this.partOfActivities(item)}> {item} </li>
+      )
+   };
+
+   nextPage = () => {
+      let {allPagesList, currentPagesList, selectedPage} = this.state;
+      let firstItem = currentPagesList[0];
+      let lastItem = currentPagesList[currentPagesList.length - 1];
+
+      if (currentPagesList[currentPagesList.length - 1] !== allPagesList[allPagesList.length - 1]) {
+         let updatedList = allPagesList.slice(firstItem, lastItem + 1);
+         this.setState({currentPagesList: updatedList, start: false});
+      }
+
+      if (currentPagesList.includes(allPagesList[allPagesList.length - 2])) {
+         this.setState({end: true});
+      }
+
+      if (selectedPage !== currentPagesList[0]) {
+         return false;
+      } else {
+         this.setState({selectedPage: currentPagesList[1]})
+      }
+      this.partOfActivities(currentPagesList[1]);
+   };
+
+   previousPage = () => {
+      let {allPagesList, currentPagesList, selectedPage} = this.state;
+      let endItem = currentPagesList[currentPagesList.length - 1];
+      let startItem = currentPagesList[0];
+      let updatedList = allPagesList.slice(startItem - 2, endItem - 1);
+      this.setState({currentPagesList: updatedList, start: false, end: false});
+
+      if (currentPagesList.includes(allPagesList[1])) {
+         this.setState({start: true});
+      }
+
+      if (selectedPage === currentPagesList[0]) {
+         this.setState({selectedPage: currentPagesList[0] - 1})
+      }
+      this.partOfActivities(currentPagesList[0] - 1);
+   };
+
+   render() {
+      const {allPagesList, start, end} = this.state;
+      return (
+           <div className={styles.paginationContainer}>
+              {
+                 allPagesList.length > 4
                       ?
-                      <li key={item} className={styles.selectedState} onClick={() =>
-                           this.partOfActivities(item)} > {item} </li>
+                      <>
+                         {start ? false : <button onClick={this.previousPage}> back </button>}
+                         <ul>
+                            {
+                               this.renderPages()
+                            }
+                         </ul>
+                         {end ? false : <button onClick={this.nextPage}> next </button>}
+                      </>
                       :
-                      <li key={item} onClick={() => this.partOfActivities(item)}> {item} </li>
-            )
-    };
+                      <ul>
+                         {
+                            this.renderPages()
+                         }
+                      </ul>
 
-    nextPage = () => {
-        let { allPagesList, currentPagesList } = this.state;
-        let firstItem = currentPagesList[0];
-        let lastItem = currentPagesList[currentPagesList.length - 1];
-
-        if(currentPagesList[currentPagesList.length - 1] !== allPagesList[allPagesList.length - 1]) {
-            let updatedList = allPagesList.slice(firstItem, lastItem + 1 );
-            // this.setState({currentPagesList: updatedList, start: false, selectedPage: updatedList[0]});
-            this.setState({currentPagesList: updatedList, start: false});
-        }
-
-        if(currentPagesList.includes(allPagesList[allPagesList.length - 2])) {
-            this.setState({end: true});
-        }
-    };
-
-    previousPage = () => {
-        let { allPagesList, currentPagesList } = this.state;
-        let endItem = currentPagesList[currentPagesList.length - 1];
-        let startItem = currentPagesList[0];
-        let updatedList = allPagesList.slice(startItem - 2, endItem - 1);
-        this.setState({currentPagesList: updatedList, start: false, end: false});
-
-        if(currentPagesList.includes(allPagesList[1])) {
-            this.setState({start: true});
-        }
-    };
-
-    render() {
-        const { allPagesList, start, end } = this.state;
-        return (
-            <div className={styles.paginationContainer}>
-                {
-                    allPagesList.length > 4
-                         ?
-                         <>
-                         { start ? false : <button onClick={this.previousPage}> back </button> }
-                         <ul>
-                             {
-                                 this.renderPages()
-                             }
-                         </ul>
-                         { end ? false : <button onClick={this.nextPage}> next </button> }
-                         </>
-                         :
-                         <ul>
-                             {
-                                 this.renderPages()
-                             }
-                         </ul>
-
-                }
-            </div>
-        );
-    }
+              }
+           </div>
+      );
+   }
 }
 
 export default Pagination;
